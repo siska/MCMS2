629cec7a8ba2ba1d237ad3ebe2e3216411af90e6
Ryan Siska <rsiska1@gmail.com>
2014-10-07 15:59:54 -0500

    Initial Commit

---
+++ MCMS2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS2
+//
+//  Created by S on 10/7/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCMS2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCMS2
+//
+//  Created by S on 10/7/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
